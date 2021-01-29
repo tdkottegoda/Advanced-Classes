a36c5ba064a7f06b78a2f47e5bd65cfc47fa683e
tdkottegoda <69494930+tdkottegoda@users.noreply.github.com>
2021-01-29 10:43:01 -0600

    Update Dog.java

--- src/advancedclasses/Dog.java
+++ src/advancedclasses/Dog.java
@@ -1,13 +1,61 @@
 package advancedclasses;
 
 
-
 /**
  * Dog.java - 
  *
  * @author Tharindu Kottegoda 
- * @since Oct. 28, 2020 
+ * @since Jan. 29, 2021 
 */
-public class Dog {
-    
+public class Dog implements Pet {
+
+    /**
+     * Default constructor, set class properties
+     */
+    public Dog() {
+        
+    }
+     
+    /**
+     * String representation of this object
+     *
+     * @return The object represented as a String
+     */
+    @Override
+    public String toString() {
+        return "Dog: " + super.toString();
+    }
+   
+    /**
+     * Deep comparison, determines if two objects are "equal" in this context
+     *
+     * @param object the object to compare to
+     * @return the objects are "equal" (true) or not (false)
+     */
+    @Override
+    public boolean equals(Object object) {
+        return super.equals(object);
+    }
+       
+    /**
+     * a Deep clone, creates a duplicate object using new memory
+     *
+     * @return a "clone" of the object using new memory
+     */
+    @Override
+    public Dog clone() {
+        return this;
+    }
+
+    @Override
+    public void setName(String name) {
+    }
+
+    @Override
+    public void getName(String name) {
+    }
+
+    @Override
+    public void sold() {
+    }
 }
